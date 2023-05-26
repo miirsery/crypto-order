1019c8fad09846c434d9f69b43c4396a2ade90ec
miirsery <miirsery.main@gmail.com>
2023-05-26 20:22:37 +0700

    fix support page

--- src/pages/support-page/index.vue
+++ src/pages/support-page/index.vue
@@ -1,20 +1,21 @@
 <template>
-  <section class="support">
+  <section class="support-page">
     <div class="support-write">
       <h3 class="support-write__title">Write to us</h3>
+
       <div class="support-write__form">
         <el-form
           ref="supportFormInstance"
-          class="support-form"
-          hide-required-asterisk
           :model="supportFormModel"
           :rules="rules"
+          class="support-form"
+          hide-required-asterisk
         >
           <div class="support-write__form-inputs">
             <el-form-item class="mb-xs-24" label="Your name" prop="name">
               <el-input v-model="supportFormModel.name" />
             </el-form-item>
-            <el-form-item class="mb-xs-47" label="E-mail" prop="email">
+            <el-form-item class="mb-xs-48" label="E-mail" prop="email">
               <el-input v-model="supportFormModel.email" type="email" />
             </el-form-item>
           </div>
@@ -30,9 +31,9 @@
           <el-form-item>
             <el-checkbox v-model="supportFormModel.agreePrivacyPolicy">
               I agree with the
-              <nuxt-link class="support-write__privacy-policy" :to="ROUTE_PATHS.PrivacyPolicy"
-                >Privacy Policy</nuxt-link
-              >
+              <nuxt-link class="support-write__privacy-policy" :to="ROUTE_PATHS.PrivacyPolicy">
+                Privacy Policy
+              </nuxt-link>
             </el-checkbox>
           </el-form-item>
           <el-form-item>
@@ -48,36 +49,35 @@
         </el-form>
       </div>
     </div>
+
     <div class="support-contacts">
       <h3 class="support-contacts__title">Contact with us</h3>
+
       <div class="support-contacts__list">
-        <div class="support-contacts__item mb-32 mb-xs-14">
+        <div class="support-contacts__item">
           <div class="support-contacts__item-main">
-            <img class="support-contacts__item-image" src="@@/assets/images/Icon-VR.png" alt="support telegram bot" />
+            <img class="support-contacts__item-image" src="@@/assets/images/Icon-VR.png" />
             <div>
               <h4 class="support-contacts__item-title">Support Telegram Bot</h4>
               <span class="support-contacts__item-description">Write here if you have any problems</span>
             </div>
           </div>
-          <div class="support-contacts__button">
-            <h4 class="support-contacts__button-text">GO</h4>
-          </div>
+          <h4 class="support-contacts__button">
+            <span class="support-contacts__button-text"> GO </span>
+          </h4>
         </div>
+
         <div class="support-contacts__item">
           <div class="support-contacts__item-main">
-            <img
-              class="support-contacts__item-image"
-              src="@@/assets/images/Icon-EmailVerification.png"
-              alt="support telegram chat"
-            />
+            <img class="support-contacts__item-image" src="@@/assets/images/Icon-EmailVerification.png" />
             <div>
               <h4 class="support-contacts__item-title">News Telegram Channel</h4>
               <span class="support-contacts__item-description">Updates, news and opening levels</span>
             </div>
           </div>
-          <div class="support-contacts__button">
-            <h4 class="support-contacts__button-text">GO</h4>
-          </div>
+          <h4 class="support-contacts__button">
+            <span class="support-contacts__button-text"> GO </span>
+          </h4>
         </div>
       </div>
     </div>
@@ -86,11 +86,8 @@
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
 import type { FormInstance, FormRules } from 'element-plus'
 import { ROUTE_PATHS } from '~/components/shared/constants'
-
-const router = useRouter()
 
 const supportFormInstance = ref<FormInstance>()
 
@@ -120,22 +117,16 @@ const handleFormSubmit = (): void => {
 </script>
 
 <style lang="scss" scoped>
-.support {
+.support-page {
   display: flex;
   justify-content: space-between;
-  margin-top: 80px;
-  margin-bottom: 112px;
+  margin: 80px 0 112px;
 
-  @include responsive(xs) {
-    flex-wrap: wrap;
-    margin-top: 28px;
-    margin-bottom: 60px;
-  }
   @include responsive(md, $breakpoints-only-max) {
-    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
     padding: 0 10px;
-    margin-top: 28px;
-    margin-bottom: 60px;
+    margin: 28px 0 60px;
   }
 }
 
@@ -146,11 +137,6 @@ const handleFormSubmit = (): void => {
   &__title {
     margin-bottom: 48px;
 
-    @include responsive(xs) {
-      &__title {
-        margin-bottom: 24px;
-      }
-    }
     @include responsive(md, $breakpoints-only-max) {
       margin-bottom: 24px;
     }
@@ -163,13 +149,6 @@ const handleFormSubmit = (): void => {
     padding: 32px;
     backdrop-filter: blur(4.5px);
 
-    @include responsive(xs) {
-      border: 1px solid rgba($color--white, 0.2);
-      border-radius: 15px;
-      background-color: $color--background-2;
-      padding: 24px;
-      backdrop-filter: blur(4.5px);
-    }
     @include responsive(md, $breakpoints-only-max) {
       border: 1px solid rgba($color--white, 0.2);
       border-radius: 15px;
@@ -199,7 +178,7 @@ const handleFormSubmit = (): void => {
   }
 
   &__privacy-policy {
-    border-bottom: 1px $color--primary solid;
+    border-bottom: 1px solid $color--primary;
     color: $color--primary;
   }
 
@@ -213,9 +192,6 @@ const handleFormSubmit = (): void => {
     padding: 14px 0;
   }
 
-  @include responsive(xs) {
-    margin-bottom: 60px;
-  }
   @include responsive(md, $breakpoints-only-max) {
     margin-bottom: 60px;
   }
@@ -224,66 +200,19 @@ const handleFormSubmit = (): void => {
 .support-contacts {
   width: 100%;
   max-width: 564px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   &__title {
     margin-bottom: 48px;
   }
 
-  &__item-title {
-    @include font(26px, 31px, 700);
-
-    margin-bottom: 14px;
-    margin-left: 32px;
-  }
-
-  &__item-description {
-    @include font(16px, 19px, 300);
-
-    letter-spacing: 0.01em;
-    color: $color--gray-9;
-    margin-left: 32px;
-  }
-
-  &__button {
-    width: 100%;
-    max-width: 78px;
-    max-height: 231px;
-    border-top-right-radius: 15px;
-    border-bottom-right-radius: 15px;
-    background: linear-gradient(112.89deg, #745bd7 25.01%, #835bd7 50.94%, #9c64e3 72.88%, #9c64e3 72.88%), #191c2e;
-    padding-top: 95px;
-    backdrop-filter: blur(4.5px);
-
-    @include responsive(xs) {
-      width: 100%;
-      height: 78px;
-      max-width: 600px;
-      border-radius: 0 0 15px 15px;
-      transform: rotate(360deg);
-      padding-top: 24px;
-    }
-    @include responsive(md, $breakpoints-only-max) {
-      width: 100%;
-      height: 78px;
-      max-width: 600px;
-      border-radius: 0 0 15px 15px;
-      transform: rotate(360deg);
-      padding-top: 24px;
-    }
-  }
-
-  &__button-text {
+  &__list {
+    height: 500px;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    transform: rotate(-90deg);
-
-    @include responsive(xs) {
-      transform: rotate(0deg);
-    }
-    @include responsive(md, $breakpoints-only-max) {
-      transform: rotate(0deg);
-    }
+    flex-direction: column;
+    justify-content: space-between;
   }
 
   &__item {
@@ -293,16 +222,29 @@ const handleFormSubmit = (): void => {
     justify-content: space-between;
     border: 1px solid rgba($color--white, 0.2);
     border-radius: 15px;
+    white-space: nowrap;
     background-color: $color--background-2;
     backdrop-filter: blur(4.5px);
 
-    @include responsive(xs) {
-      width: 100%;
-      height: 309px;
-      display: flex;
-      flex-direction: column;
-      align-content: space-between;
+    &-title {
+      @include font(26px, 31px, 700);
+
+      margin-bottom: 14px;
+      margin-left: 32px;
     }
+
+    &-description {
+      @include font(16px, 19px, 300);
+
+      letter-spacing: 0.01em;
+      color: $color--gray-9;
+      margin-left: 32px;
+    }
+
+    &-image {
+      padding-left: 10px;
+    }
+
     @include responsive(md, $breakpoints-only-max) {
       width: 100%;
       height: 309px;
@@ -312,8 +254,32 @@ const handleFormSubmit = (): void => {
     }
   }
 
-  &__item-image {
-    padding-left: 10px;
+  &__button {
+    width: 78px;
+    height: 231px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0 15px 15px 0;
+    background: linear-gradient(112.89deg, #745bd7 25.01%, #835bd7 50.94%, #9c64e3 72.88%, #9c64e3 72.88%), #191c2e;
+    backdrop-filter: blur(4.5px);
+
+    &-text {
+      display: block;
+      transform: rotate(-90deg);
+
+      @include responsive(md, $breakpoints-only-max) {
+        transform: rotate(0deg);
+      }
+    }
+
+    @include responsive(md, $breakpoints-only-max) {
+      width: 100%;
+      height: 78px;
+      max-width: 600px;
+      border-radius: 0 0 15px 15px;
+      transform: rotate(360deg);
+    }
   }
 }
 </style>
